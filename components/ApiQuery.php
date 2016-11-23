e9c6d2221f2b07a6eec19b10b6b53d18bde3edc7
andkon <andkon13@mail.ru>
2016-11-23 08:36:14 +0300

    Создаем ApiQuery

--- components/ApiQuery.php
+++ components/ApiQuery.php
@@ -19,5 +19,88 @@ use yii\db\QueryTrait;
  */
 class ApiQuery implements QueryInterface
 {
+    use QueryTrait;
+    /**
+     * @var string|ApiRecord
+     */
+    protected $class;
 
+    public $multiple = true;
+
+    /**
+     * ApiQuery constructor.
+     *
+     * @param $className
+     */
+    public function __construct($className)
+    {
+        $this->class = $className;
+    }
+
+    /**
+     * @param null $db
+     *
+     * @return array
+     */
+    public function all($db = null)
+    {
+        $result = Api::request((new QueryBuilder())->build($this, $this->class));
+        $items  = $this->createModels($result);
+
+        return $items;
+    }
+
+    /**
+     * @param null $db
+     *
+     * @return mixed
+     */
+    public function one($db = null)
+    {
+        $this->limit(1);
+        $result = Api::request((new QueryBuilder)->build($this, $this->class));
+        $items  = $this->createModels($result);
+
+        return array_shift($items);
+    }
+
+    /**
+     * @param string $q
+     * @param null   $db
+     *
+     * @return void
+     */
+    public function count($q = '*', $db = null)
+    {
+        // TODO: Implement count() method.
+    }
+
+    /**
+     * @param null $db
+     *
+     * @return bool
+     */
+    public function exists($db = null)
+    {
+        $this->limit(1);
+        $result = Api::request((new QueryBuilder)->build($this, $this->class));
+        $items  = $this->createModels($result);
+
+        return 0 < count($items);
+    }
+
+    /**
+     * @param $data
+     *
+     * @return ApiRecord[]
+     */
+    protected function createModels($data)
+    {
+        $models = [];
+        foreach ($data as $item) {
+            $models[] = ($this->class)::fabric($item);
+        }
+
+        return $models;
+    }
 }
