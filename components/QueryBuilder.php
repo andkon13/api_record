f149bac9b11d2b52f9b3c8e1654824c2b87eb729
andkon <andkon13@mail.ru>
2016-11-23 08:29:14 +0300

    Создаем QueryBuilder

--- components/QueryBuilder.php
+++ components/QueryBuilder.php
@@ -18,4 +18,62 @@ use app\models\Provider;
  */
 class QueryBuilder
 {
+    static protected $pathes = [
+        Provider::class => 'providers',
+        Product::class  => 'items',
+    ];
+
+    /**
+     * @param $className
+     *
+     * @return mixed
+     */
+    protected function preparePath($className)
+    {
+        return self::$pathes[$className];
+    }
+
+    /**
+     * @param ApiQuery $query
+     * @param string   $className
+     *
+     * @return string
+     */
+    public function build($query, $className)
+    {
+        $queryString = $this->preparePath($className);
+        $queryString .= $this->prepareApiParams($query->where);
+
+        return $queryString;
+    }
+
+    /**
+     * Формирует строку запроса к апи
+     *
+     * @param array $params
+     * @param bool  $isOr
+     *
+     * @return string
+     */
+    public function prepareApiParams($params = [], $isOr = false)
+    {
+        $tmpParam = [];
+        if (!is_array($params)) {
+            return '';
+        }
+
+        foreach ($params as $param) {
+            if (is_array($param)) {
+                $tmpParam[] = $this->prepareApiParams($param, ('"or"' === ($tmpParam[0] ?? false)));
+            } else {
+                $tmpParam[] = '"' . $param . '"';
+            }
+        };
+
+        if ($isOr && false !== strpos($tmpParam[0], '[')) {
+            return implode(',', $tmpParam);
+        }
+
+        return '[' . implode(',', $tmpParam) . ']';
+    }
 }
